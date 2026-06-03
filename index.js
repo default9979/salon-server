4efd4b8d20e7fbb30c8905b18e439fb926fb6b86
Danil <relaxmusicventi@gmail.com>
2026-06-03 17:50:44 +0300

    починил порт для Railway

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.post('/send', async function(req, res) {
   res.json({ ok: true });
 });
 
-app.listen(3000, function() {
-  console.log('Сервер запущен на порту 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+  console.log('Сервер запущен на порту ' + PORT);
 });
